dc57ee33eb31b8a68fa33a3a3f7f0362b067c932
Mariá Calmon Dombrate <mari.dombrate@hotmail.com>
2022-09-15 13:04:50 -0300

    Criando Blog

---
+++ src/components/estaticos/footer/Footer.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Grid, Typography } from "@material-ui/core";
+import { Box } from "@mui/material";
+import "./Footer.css";
+
+
+function Footer (){
+    return (
+       <>
+       <h1>Footer</h1>
+       </> 
+    )
+}
+
+export default Footer;
---
+++ src/components/estaticos/navbar/Navbar.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Grid, Typography } from "@material-ui/core";
+import { Box } from "@mui/material";
+import "./Navbar.css";
+
+function Navbar (){
+    return (
+       <>
+       <h1>Navbar</h1>
+       
+       </> 
+    )
+}
+
+export default Navbar;
