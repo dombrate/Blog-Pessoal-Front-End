3540c204782267eeb115b1231287189b33f90682
Mariá Calmon Dombrate <mari.dombrate@hotmail.com>
2022-09-16 03:15:49 -0300

    blog atualizado com crud e toastify

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,53 @@
 import React from 'react';
+import './App.css';
+import 'react-toastify/dist/ReactToastify.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import Login from './paginas/login/Login';
+import CadastroUsuario from './paginas/cadastroUsuario/CadastroUsuario';
 import Navbar from './components/estaticos/navbar/Navbar';
+import models from './paginas/home/Home';
+import CadastroPostagem from './components/postagens/cadastroPost/CadastroPostagem';
+import DeletarPostagem from './components/postagens/deletarPostagem/DeletarPostagem';
+import ListaPostagem from './components/postagens/listapostagem/ListaPostagem';
+import CadastroTema from './components/temas/cadastroTema/CadastroTema';
+import DeletarTema from './components/temas/deletarTema/DeletarTema';
+import ListaTema from './components/temas/listatema/ListaTema';
 import Footer from './components/estaticos/footer/Footer';
-import{Grid} from '@material-ui/core';
+import store from './store/store';
+import { ToastContainer } from 'react-toastify';
+
 import Home from './paginas/home/Home';
 import './App.css';
 
 function App() {
   return (
-<>
-<Navbar />
-<Home />
-<Footer />
-</>
+    <Provider store={store}>
+      <ToastContainer />
+      <Router>
+        <Navbar />
+        <div style={{ minHeight: '100vh' }}>
+          <Routes>
+            <Route path="/" element={<Login />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/cadastro" element={<CadastroUsuario />} />
+            <Route path="/home" element={<Home />} />
+            <Route path="/temas" element={<ListaTema />} />
+            <Route path="/postagens" element={<ListaPostagem />} />
 
+            <Route path="/formularioTema" element={<CadastroTema />} />
+            <Route path="/formularioTema/:id" element={<CadastroTema />} />
+
+            <Route path="/formularioPostagem" element={<CadastroPostagem />} />
+            <Route path="/formularioPostagem/:id" element={<CadastroPostagem />} />
+
+            <Route path="/deletarPostagem/:id" element={<DeletarPostagem />} />
+            <Route path="/deletarTema/:id" element={<DeletarTema />} />
+          </Routes>
+        </div>
+        <Footer />
+      </Router>
+    </Provider >
   );
 }
 
